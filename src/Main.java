8d2182a05817f1bd91b62b9be4145c6ef6c691fd
lucasmkrolow <lucaskrolowcba@gmail.com>
2022-07-23 11:10:19 -0400

    Commit Inicial

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Cliente lucas = new Cliente();
+		lucas.setNome("Lucas");
+		
+		Conta cc = new ContaCorrente(lucas);
+		Conta poupanca = new ContaPoupanca(lucas);
+		
+		cc.depositar(100);
+		cc.transferir(50, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+	}
+	
+}
